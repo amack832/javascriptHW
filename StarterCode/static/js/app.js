8b38ca0fd5793d76e79668843f55f2d0bec69bb3
aaaa <amack832@gmail.com>
2019-01-23 10:02:43 -0600

    code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,12 +8,12 @@ var tbody = document.querySelector("tbody");
 function makeTable () {
 	tbody.innerHTML = "";
 	for (var i = 0; i < tableData.length; i++) {
-		var ufoData = tableData[i];
-		var fields = Object.keys(ufoData);
+		var ufoD = tableData[i];
+		var fields = Object.keys(ufoD);
 		var row = tbody.insertRow(i);
 		for (var j=0; j < fields.length; j++){
 			var dataCell = row.insertCell(j);
-			dataCell.innerHTML = ufoData[fields[j]];
+			dataCell.innerHTML = ufoD[fields[j]];
 		}
 		
 	}
